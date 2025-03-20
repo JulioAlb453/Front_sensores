094dc908b5d0f92864ce2a3e129932fa222414c2
JulioAlb453 <fulioibeto18@gmail.com>
2025-03-20 10:47:32 -0600

    add chart data temperature and module for sensor temperature

--- src/app/sensors/sensor-view/sensor-view.module.ts
+++ src/app/sensors/sensor-view/sensor-view.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { Sensor1DisplayComponent } from '../sensor1/sensor1-display/sensor1-display.component';
 import { Sensor2DisplayComponent } from '../sensor2/sensor2-display/sensor2-display.component';
 import { Sensor3DisplayComponent } from '../sensor3/sensor3-display/sensor3-display.component';
 import { SensorViewComponent } from './sensor-view.component';
-import { SensorTemperatureComponent } from '../sensor1/sensor-temperature/sensor-temperature.component';
+import { Sensor1Module } from '../sensor1/sensor1.module';
 import { SensorNoiseComponent } from '../sensor2/sensor-noise/sensor-noise.component';
 import { SensorLighComponent } from '../sensor3/sensor-ligh/sensor-ligh.component';
 
+import { NgApexchartsModule } from 'ng-apexcharts';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
@@ -17,10 +17,8 @@ import { RouterLink } from '@angular/router';
 @NgModule({
   declarations: [
     SensorViewComponent,
-    Sensor1DisplayComponent,
     Sensor2DisplayComponent,
     Sensor3DisplayComponent,
-    SensorTemperatureComponent,
     SensorNoiseComponent,
     SensorLighComponent,
   ],
@@ -30,6 +28,8 @@ import { RouterLink } from '@angular/router';
     MatButtonModule,
     MatDividerModule,
     RouterLink,
+    NgApexchartsModule,
+    Sensor1Module,
   ],
 })
 export class SensorViewModule {}
--- src/app/sensors/sensor1/sensor-temperature/sensor-temperature.component.ts
+++ src/app/sensors/sensor1/sensor-temperature/sensor-temperature.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sensor-temperature',
-  standalone:false,
+  standalone: false,
   templateUrl: './sensor-temperature.component.html',
-  styleUrl: './sensor-temperature.component.css'
+  styleUrl: './sensor-temperature.component.css',
 })
 export class SensorTemperatureComponent {
 
+  
 }
