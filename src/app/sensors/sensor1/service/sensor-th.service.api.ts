3d30f01579026b7587369de23fe2409b333213e6
JulioAlb453 <fulioibeto18@gmail.com>
2025-04-02 11:57:42 -0600

    add model into temperature sensor

---
+++ src/app/sensors/sensor1/service/sensor-th.service.api.ts
@@ -0,0 +1,18 @@
+// src/app/features/sensor-th/infrastructure/sensor-th-api.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { TemperatureHumiditySensor } from '../domain/temperature-humidity.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class SensorThApiService {
+  private readonly baseUrl = environment.API_URl_Consumer;
+
+  constructor(private http: HttpClient) {}
+
+  getSensorData(): Observable<TemperatureHumiditySensor> {
+    return this.http.get<TemperatureHumiditySensor>(`${this.baseUrl}/temperaturehumidity/data`);
+  }
+}
